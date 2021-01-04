e4a172f2abcad085b338b88f7420cdf4b522c090
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-04 19:05:05 +0530

    Added the Navbar.js

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark p-3 text-center mb-4">
+            <div className="navbar-brand mx-auto">
+                PixaBay Image Search
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
